206ba65019226010dbf4d0a0a2d8a0e6d43724ce
Andy Niccolai <andy.niccolai@gmail.com>
2014-12-26 15:04:24 -0600

    partial refactor

--- command_to_patch.js
+++ command_to_patch.js
@@ -1,48 +1,51 @@
-var pointer = require('json-pointer');
-var _ = require('lodash');
+module.exports = createPatcher([
+  storyId,
+  storyCreatedAt,
+  storyUpdatedAt,
+  storyAcceptedAt,
+  storyAttr('accepted_at'),
+  storyAttr('estimate'),
+  storyAttr('story_type'),
+  storyAttr('name'),
+  storyAttr('description'),
+  storyAttr('current_state'),
+  storyAttr('requested_by_id'),
+  storyAttr('owner_ids'),
+  storyAttr('label_ids'),
+  storyAttr('tasks'),
+  storyAttr('follower_ids'),
+  storyAttr('comments'),
+  storyAttr('owned_by_id'),
+  storyMoves,
+  projectVersion
+]);
 
-module.exports = function(project, command) {
+function storyId(project, command) {
+  return patchStoryAttr('id', project, command);
+}
+
+function storyCreatedAt(project, command) {
+  return patchStoryAttr('created_at', project, command);
+}
+
+function storyUpdatedAt(project, command) {
+  return patchStoryAttr('updated_at', project, command);
+}
+
+function storyAcceptedAt(project, command) {
+  return patchStoryAttr('accepted_at', project, command);
+}
+
+function projectVersion(project, command) {
+  return [{op: 'replace', path: '/version', value: command.project.version}];
+}
+
+function storyMoves(project, command) {
   var patch = [];
-  var results = command.results;
-  var resultsLen = results.length;
 
-  for (var i = 0; i < resultsLen; i++) {
-    var result = results[i]
+  command.results.forEach(function(result) {
     var index = indexOfStory(project, result.id);
-    var original = project.stories[index];
     var storyPath = '/stories/' + index;
-
-    function patchAttr(attr) {
-      if (result[attr]) {
-        if (!original[attr]) {
-          patch.push({op: 'add', path: storyPath + '/' + attr, value: result[attr]});
-        }
-        else if (result[attr] !== original[attr]) {
-          patch.push({op: 'replace', path: storyPath + '/' + attr, value: result[attr]});
-        }
-      }
-    }
-
-    if (result.created_at) {
-      patch.push()
-    }
-
-    patchAttr('id');
-    patchAttr('created_at');
-    patchAttr('updated_at');
-    patchAttr('accepted_at');
-    patchAttr('estimate');
-    patchAttr('story_type');
-    patchAttr('name');
-    patchAttr('description');
-    patchAttr('current_state');
-    patchAttr('requested_by_id');
-    patchAttr('owner_ids');
-    patchAttr('label_ids');
-    patchAttr('tasks');
-    patchAttr('follower_ids');
-    patchAttr('comments');
-    patchAttr('owned_by_id');
 
     if (result.after_id) {
       var afterIndex = indexOfStory(project, result.after_id) + 1;
@@ -52,12 +55,62 @@ module.exports = function(project, command) {
       var beforeIndex = indexOfStory(project, result.before_id);
       patch.push({op: 'move', path: '/stories/' + beforeIndex, from: storyPath});
     }
-  }
-
-  patch.push({op: 'replace', path: '/version', value: command.project.version});
+  });
 
   return patch;
-};
+}
+
+function patchStoryAttr(attr, project, command) {
+  var patch = [];
+
+  command.results.forEach(function(result) {
+    var index = indexOfStory(project, result.id);
+    var storyPath = '/stories/' + index;
+    var original = project.stories[index];
+
+    if (result[attr]) {
+      if (!original[attr]) {
+        patch.push({op: 'add', path: storyPath + '/' + attr, value: result[attr]});
+      }
+      else if (result[attr] !== original[attr]) {
+        patch.push({op: 'replace', path: storyPath + '/' + attr, value: result[attr]});
+      }
+    }
+  });
+
+  return patch;
+}
+
+function storyAttr(attr) {
+  return function(project, command) {
+    var patch = [];
+
+    command.results.forEach(function(result) {
+      var index = indexOfStory(project, result.id);
+      var storyPath = '/stories/' + index;
+      var original = project.stories[index];
+
+      if (result[attr]) {
+        if (!original[attr]) {
+          patch.push({op: 'add', path: storyPath + '/' + attr, value: result[attr]});
+        }
+        else if (result[attr] !== original[attr]) {
+          patch.push({op: 'replace', path: storyPath + '/' + attr, value: result[attr]});
+        }
+      }
+    });
+
+    return patch;
+  }
+}
+
+function createPatcher(patchers) {
+  return function(project, command) {
+    return patchers.reduce(function(patch, patcher) {
+      return patch.concat(patcher(project, command));
+    }, []);
+  }
+}
 
 function indexOfStory(project, id) {
   var stories = project.stories;
