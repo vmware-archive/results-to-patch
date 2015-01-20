a954a061d11e2ff1093fd90f461642de74135a26
Andy Niccolai <andy.niccolai@gmail.com>
2015-01-20 02:50:14 -0700

    add snapshots from itegration test run

--- test/story_update_test.js
+++ test/story_update_test.js
@@ -8,7 +8,7 @@ var commandToPatch   = require('../command_to_patch.js');
 chai.Assertion.addMethod('patch', patchAssertion);
 
 var snapshots = fs.readdirSync('./test/fixtures');
-// var snapshots = ['StoryCreate_1e5862'];
+// var snapshots = ['MultiLabelCreate_9c5485'];
 
 snapshots.forEach(function(name) {
   it('converts ' + name + ' to JSON patch', function() {
