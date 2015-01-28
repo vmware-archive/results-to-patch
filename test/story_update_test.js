15147c5eb046085bf56d0c364c29ed5e1597e91e
Andy Niccolai <andy.niccolai@gmail.com>
2015-01-28 13:38:34 -0700

    move failures into separate dir

--- test/story_update_test.js
+++ test/story_update_test.js
@@ -8,7 +8,7 @@ var commandToPatch   = require('../command_to_patch.js');
 chai.Assertion.addMethod('patch', patchAssertion);
 
 var snapshots = fs.readdirSync('./test/fixtures').filter(function(f) { return f !== '.DS_Store' });
-// var snapshots = ['MultiStoryDelete_06f881'];
+// var snapshots = ['MultiStoryMoveFromProject_243f3b'];
 
 snapshots.forEach(function(name) {
   it('converts ' + name + ' to JSON patch', function() {
