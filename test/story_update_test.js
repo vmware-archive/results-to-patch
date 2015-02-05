6619c80c56eec8869fb92b21159c368677e501b5
Andy Niccolai <andy.niccolai@gmail.com>
2015-02-05 10:55:13 -0700

    compress patches

--- test/story_update_test.js
+++ test/story_update_test.js
@@ -4,12 +4,12 @@ var expect           = chai.expect;
 var patchAssertion   = require('./patch_assertion');
 var helper           = require('./helper');
 var fs               = require('fs');
-var commandToPatch   = require('../command_to_patch.js');
+var commandToPatch   = require('../lib/convert.js');
+var compress         = require('../lib/compress.js');
 
 chai.Assertion.addMethod('patch', patchAssertion);
 
 var snapshots = fs.readdirSync('./test/fixtures').filter(function(f) { return f !== '.DS_Store' });
-// var snapshots = ['StoryUpdate_fe0ca7'];
 
 snapshots.forEach(function(name) {
   it('creates a patch for ' + name, function() {
@@ -18,6 +18,8 @@ snapshots.forEach(function(name) {
     var after = snapshot.after;
     var command = snapshot.command.stale_commands[0];
     var patch = commandToPatch(before, command).forward;
+
+    patch = compress(patch);
 
     expect(patch).to.patch(before, after);
   });
@@ -29,8 +31,8 @@ snapshots.forEach(function(name) {
     var before = snapshot.before;
     var after = snapshot.after;
     var command = snapshot.command.stale_commands[0];
-    var patch = commandToPatch(before, command).reverse;
+    var patch = commandToPatch(before, command);
 
-    expect(patch).to.patch(after, before);
+    expect(patch.reverse).to.patch(after, before);
   });
 });
