e6dcdc2c99e7eab8324010e84cda4f833f745f93
Andy Niccolai <aniccolai@pivotal.io>
2015-03-04 07:42:34 -0700

    wip

--- test/patch_assertion.js
+++ test/patch_assertion.js
@@ -1,11 +1,41 @@
+var _      = require('lodash');
 var chai   = require('chai');
 var helper = require('./helper');
+
+function omit(json) {
+  if (_.isArray(json)) {
+    return json.map(omit);
+  }
+
+  if (_.isObject(json)) {
+    json = _.omit(json, 'version', 'updated_at', 'accepted_at');
+
+    for (var prop in json) {
+      json[prop] = omit(json[prop]);
+    }
+    return json;
+  }
+
+  return json;
+}
 
 function patch(before, after) {
   var actual = this._obj;
   var patched = helper.patch(before, actual);
-  // console.log('patched', JSON.stringify(patched, null, '\t'))
   new chai.Assertion(patched).to.deep.equal(after);
 }
 
-module.exports = patch;
+function patchLike(before, after) {
+  var actual = this._obj;
+  var patched = helper.patch(before, actual);
+
+  after = omit(after);
+  patched = omit(patched);
+
+  new chai.Assertion(patched).to.deep.equal(after);
+}
+
+module.exports = {
+  patch: patch,
+  patchLike: patchLike,
+};
--- test/story_update_test.js
+++ test/story_update_test.js
@@ -1,25 +1,42 @@
-var _                = require('lodash');
-var chai             = require('chai');
-var expect           = chai.expect;
-var patchAssertion   = require('./patch_assertion');
-var helper           = require('./helper');
-var commandToPatch   = require('../lib/convert.js');
-var compress         = require('../lib/compress.js');
-chai.Assertion.addMethod('patch', patchAssertion);
+var _               = require('lodash');
+var chai            = require('chai');
+var expect          = chai.expect;
+var patchAssertion  = require('./patch_assertion');
+var helper          = require('./helper');
+var commandToPatch  = require('../lib/convert');
+var translateParams = require('../lib/param-executor');
+var compress        = require('../lib/compress');
+chai.Assertion.addMethod('patch', patchAssertion.patch);
+chai.Assertion.addMethod('patchLike', patchAssertion.patchLike);
 
-var FIXTURES = './test/fixtures/**/*.json';
+// var FIXTURES = './test/fixtures/**/*.json';
+// var FIXTURES = './test/fixtures/*Delete_*/*.json';
+// var FIXTURES = './test/fixtures/*Update_*/*.json';
+var FIXTURES = './test/fixtures/StoryUpdate_*/*.json';
+// var FIXTURES = './test/fixtures/StoryUpdate_ff296a*/*.json';
 
 helper.snapshots(FIXTURES).forEach(function(snapshot) {
   it('creates a patch for ' + snapshot.name, function() {
     var before  = snapshot['before.json'];
     var after   = snapshot['after.json'];
     var command = snapshot['command.json'].stale_commands[0];
-    var patch   = commandToPatch(before, command).forward;
-    patch = compress(patch);
+    var patch   = translateParams(before, command).forward;
 
-    expect(patch).to.patch(before, after);
+    expect(patch).to.patchLike(before, after);
   });
 });
+
+// helper.snapshots(FIXTURES).forEach(function(snapshot) {
+//   it('creates a patch for ' + snapshot.name, function() {
+//     var before  = snapshot['before.json'];
+//     var after   = snapshot['after.json'];
+//     var command = snapshot['command.json'].stale_commands[0];
+//     var patch   = commandToPatch(before, command).forward;
+//     patch = compress(patch);
+
+//     expect(patch).to.patch(before, after);
+//   });
+// });
 
 // snapshots.forEach(function(name) {
 //   it('creates a reverse patch for ' + name, function() {
