1aa2081b667b6f884e67db8ec977453e1f6d12e5
Andy Niccolai <pair+aniccolai@pivotal.io>
2015-10-05 22:26:37 -0600

    move more into reducers

--- test/story_update_test.js
+++ test/story_update_test.js
@@ -6,7 +6,8 @@ var helper           = require('./helper');
 var resultsToPatch   = require('../lib');
 chai.Assertion.addMethod('patch', patchAssertion);
 
-// var FIXTURES = './test/fixtures/MultiStoryDelete*/*.json';
+// var FIXTURES = './test/fixtures/MultiStoryMoveIntoProjectAndPrioritize_6482f5/*.json';
+// var FIXTURES = './test/fixtures/CommentDelete*/*.json';
 var FIXTURES = './test/fixtures/**/*.json';
 
 helper.snapshots(FIXTURES).forEach(function(snapshot) {
@@ -16,9 +17,21 @@ helper.snapshots(FIXTURES).forEach(function(snapshot) {
     var command = snapshot['command.json'].stale_commands[0];
 
     var state = resultsToPatch(before, command);
-    expect(state).to.deep.equal(after);
 
-    // var patch   = resultsToPatch(before, command).forward;
-    // expect(patch).to.patch(before, after);
+    expect(state).to.deep.equal(after);
+    // expect(state.stories.map(formatPosition)).to.deep.equal(after.stories.map(formatPosition));
   });
 });
+
+function idToIndex(memo, story, index, array) {
+  memo[story.id] = {
+    index: index,
+    before_id: (array[index + 1] || {}).id,
+    after_id: (array[index - 1] || {}).id,
+  };
+  return memo;
+}
+
+function formatPosition(story, index) {
+  return story.id;
+}
